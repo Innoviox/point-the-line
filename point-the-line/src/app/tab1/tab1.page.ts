c909768a9727961cd5edfd6d632229e54f2029b4
Simon Chervenak <sarras305@gmail.com>
2022-10-05 12:29:47 -0400

    yeah fe im just gonna do swift

--- point-the-line/src/app/tab1/tab1.page.ts
+++ point-the-line/src/app/tab1/tab1.page.ts
@@ -1,3 +1,5 @@
+// device orientation code from https://ionicframework.com/docs/native/device-orientation
+import { DeviceOrientation, DeviceOrientationCompassHeading } from '@awesome-cordova-plugins/device-orientation/ngx';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  orientation = 0;
+  private subscription;
 
-  constructor() {}
+  constructor(private deviceOrientation: DeviceOrientation) {}
 
+  ngOnInit(): void {
+    this.deviceOrientation.getCurrentHeading().then(
+      (data: DeviceOrientationCompassHeading) => console.log(data),
+      (error: any) => console.log(error)
+    );
+
+    this.subscription = this.deviceOrientation.watchHeading().subscribe(
+      (data: DeviceOrientationCompassHeading) => console.log(data)
+    );
+  }
 }
