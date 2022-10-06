26a3e8ee827b44613e45c7bf2c2a5491844091ca
Simon Chervenak <sarras305@gmail.com>
2022-10-06 12:59:07 -0400

    ok well now i can't simulate

--- point2/point2/ViewController.swift
+++ point2/point2/ViewController.swift
@@ -6,18 +6,34 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     private var lineView: LineView!
+    private var locationManager = CLLocationManager()
     
-
+    @IBOutlet weak var direction: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.lineView = LineView(x: self.view.frame.width / 2, y: self.view.frame.origin.y, width: 10, height: self.view.frame.height)
         self.view.addSubview(self.lineView)
+        
+        if (CLLocationManager.headingAvailable()) {
+            locationManager.headingFilter = 1
+            locationManager.startUpdatingHeading()
+            locationManager.delegate = self
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading heading: CLHeading) {
+        direction.text = heading.magneticHeading
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
