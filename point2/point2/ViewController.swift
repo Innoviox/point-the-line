2eb6f03f689771209e5183c98a7d70485c42b310
Simon Chervenak <sarras305@gmail.com>
2022-10-07 11:24:03 -0400

    probably dont need emory warnings
    clean code as they say

--- point2/point2/ViewController.swift
+++ point2/point2/ViewController.swift
@@ -9,15 +9,19 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     private var lineView: LineView!
     private var locationManager = CLLocationManager()
 
     private var currentPlaceAnnotation = MKPointAnnotation()
+    private var last_line: MKPolyline?
     
     @IBOutlet weak var map: MKMapView!
     
     private var zoomed = false
+//    private var addedLine = false
+    
+    private var last_center: CLLocationCoordinate2D?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,17 +43,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
         
 //        map.addAnnotation(currentPlaceAnnotation)
-        map.showsUserLocation = true;
+        map.showsUserLocation = true
+        map.delegate = self
     }
-    
+}
+
+extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateHeading heading: CLHeading) {
 //        direction.text = "Direction: \(heading.magneticHeading)"
+        
+        guard let center = last_center else {
+            return
+        }
+        
+        if let line = last_line {
+            map.removeOverlay(line)
+        }
+        
+        last_line = MKPolyline(coordinates: [center, center], count: 2)
+        map.addOverlay(last_line!)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.last{
+        if let location = locations.last {
             let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            
             if (!zoomed) {
                 zoomed = true
                 let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
@@ -57,12 +74,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             } else {
                 self.map.setCenter(center, animated: true)
             }
+            
+            last_center = center
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let polyline = overlay as? MKPolyline {
+            let testlineRenderer = MKPolylineRenderer(polyline: polyline)
+            testlineRenderer.strokeColor = .blue
+            testlineRenderer.lineWidth = 2.0
+            return testlineRenderer
+        }
+        fatalError("Something wrong...")
+    }
+}
